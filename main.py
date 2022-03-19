8736af87a5fa69b361346cd7012015b848370bc9
Kamyyyk <kamyk962@gmail.com>
2022-03-19 17:00:37 +0100

    wip

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 URL = 'https://www.olx.pl/noclegi/'
 
+
 # OLX
 div_ogloszenia = 'offer-wrapper'
 stopka_ogloszenia = 'bottom-cell'
@@ -13,7 +14,8 @@ cena = 'price'
 
 # Iteracja po strukturze strony
 def page_parser(num):
-    print(f'\nStrona {num}:')
+    f = open(f'Strona{num}.txt', 'w')
+    f.write(f'Strona {num}:\n')
     page_olx = requests.get(f'{URL}?page={num}')
     bs = BeautifulSoup(page_olx.content, 'html.parser')
     for offer in bs.find_all('div', div_ogloszenia):
@@ -22,8 +24,8 @@ def page_parser(num):
         name = offer.find('strong').get_text().strip()
         price = offer.find('p', cena).get_text().strip()
         link = offer.find('a')
-        print(f'{name} Miejscowosc: {place} Cena: {price}')
-        print(link['href'])
+        f.write(f'{name} Miejscowosc: {place} Cena: {price}\n')
+        f.write(f'{link["href"]}\n')
 
 
 if __name__ == '__main__':
